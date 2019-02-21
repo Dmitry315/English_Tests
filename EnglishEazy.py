df7af30908bdf5a9573b9d1cd6cdd219fb44684e
Dmitry315 <vgurskoy@mail.ru>
2019-02-21 15:26:23 +0300

    add home page

--- EnglishEazy.py
+++ EnglishEazy.py
@@ -1,15 +1,45 @@
 from forms import SignInForm, LoginForm
 from models import *
 
+
+# init data bases from models
 db.create_all()
 
+# return user's login if session is not empty
+def check_session():
+    try:
+        if not(UserModel.query.filter_by(username=session['username']).first()):
+            session['username'] = None
+        return session['username']
+    except KeyError as err:
+        return False
 
-
+# home page with nav bar
+# if session is empty redirect to log in
 @app.route('/')
 def home():
-    return render_template('home.html')
+    is_loged = check_session()
+    if not is_loged:
+        return redirect('/log_in')
+    return render_template('home.html', user=UserModel.query.filter_by(username=is_loged).first())
 
-@app.route('/login', methods=['GET', 'POST'])
+@app.route('/profile/<int:id>')
+def profile(id):
+    is_loged = check_session()
+    if not is_loged:
+        return redirect('/log_in')
+    return render_template('profile.html', user=UserModel.query.filter_by(id=id).first())
+
+# remove user from session
+# and redirect to log in
+@app.route('/log_out')
+def log_out():
+    session['username'] = None
+    return redirect('/log_in')
+
+# insert user in session
+# and redirect to home page
+@app.route('/log_in', methods=['GET', 'POST'])
 def login():
     form = LoginForm()
     if form.validate_on_submit():
@@ -20,13 +50,15 @@ def login():
         user = UserModel.query.filter_by(username = login).first()
         if bool(user):
             form.login.errors = []
-            if password != str(user).split()[2]:
+            if password != user.get_password():
                 form.password.errors = ['Wrong password']
-
         if not form.login.errors and not form.password.errors:
+            session['username'] = login
             return redirect('/')
     return render_template('login.html', form=form)
 
+# sign user in system
+# and redirect to authorisation
 @app.route('/sign_in', methods=['GET', 'POST'])
 def sign_in():
     form = SignInForm()
@@ -37,19 +69,25 @@ def sign_in():
         login = request.form['login']
         password = request.form['password']
         re_password = request.form['re_password']
+        # try to pull user from date
+        # to check login
         user = UserModel.query.filter_by(username = login).first()
+        # check input information
         if bool(user):
             form.login.errors = ['User with this login already exsist.']
             return render_template('sign_in.html', form=form)
         if password != re_password:
             form.re_password.errors = ['Your passwords differ.']
             return render_template('sign_in.html', form=form)
+        # add user to date base and commit changes
         db.session.add(UserModel(
             username=login,
             password=password,
+            about='',
+            links='',
         ))
         db.session.commit()
-        return redirect('/login')
+        return redirect('/log_in')
     return render_template('sign_in.html', form=form)
 
 if __name__ == '__main__':
--- models.py
+++ models.py
@@ -1,16 +1,37 @@
 from flask_sqlalchemy import SQLAlchemy
-from flask import Flask, render_template, redirect, request
+from flask import Flask, render_template, redirect, request, session
 
+# init app and configure server
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '1515dd15dd3d5d1a51b5af515ca'
+
+# add data base
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
+# add User model
 class UserModel(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(80), unique=True, nullable=False)
-    password = db.Column(db.String(80), unique=False, nullable=False)
+    username = db.Column(db.String(64), unique=True, nullable=False)
+    password = db.Column(db.String(64), unique=False, nullable=False)
+    about = db.Column(db.String(256), unique=False, nullable=True)
+    links = db.Column(db.String(64), unique=False, nullable=True)
 
     def __repr__(self):
         return '<User {} {} {}>'.format(self.id, self.password, self.username)
+
+    def get_description(self):
+        return self.about
+
+    def get_links(self):
+        return self.links
+
+    def get_id(self):
+        return self.id
+
+    def get_name(self):
+        return self.username
+
+    def get_password(self):
+        return self.password
